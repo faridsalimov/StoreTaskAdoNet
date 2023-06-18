f9d73430661cad0347bf6be0546ac735a74cff71
Farid Salimov <selimovferid85@gmail.com>
2023-06-18 21:16:36 +0400

    Task

---
+++ StoreTaskAdoNet/Domain/ViewModel/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using StoreTaskAdoNet.Commands;
+using StoreTaskAdoNet.DataAccess.SqlServer;
+using StoreTaskAdoNet.Domain.Services;
+using StoreTaskAdoNet.Domain.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreTaskAdoNet.Domain.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand ProductsCommand { get; set; }
+        public RelayCommand OrdersCommand { get; set; }
+        public RelayCommand RefreshCommand { get; set; }
+        public RelayCommand SelectProductCommand { get; set; }
+
+        private readonly ProductService _productService;
+        private ObservableCollection<Product> allProducts;
+        public ObservableCollection<Product> AllProducts
+        {
+            get { return allProducts; }
+            set { allProducts = value; OnPropertyChanged(); }
+        }
+
+        private Product selectedProduct;
+        public Product SelectedProduct
+        {
+            get { return selectedProduct; }
+            set { selectedProduct = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            _productService = new ProductService();
+            AllProducts = _productService.GetAllProducts();
+
+            ProductsCommand = new RelayCommand((obj) =>
+            {
+                var vm = new ProductsViewModel();
+                var view = new Products();
+                view.DataContext = vm;
+                view.ShowDialog();
+            });
+
+            OrdersCommand = new RelayCommand((obj) =>
+            {
+                var vm = new OrdersViewModel();
+                var view = new Orders();
+                view.DataContext = vm;
+                view.ShowDialog();
+            });
+
+            RefreshCommand = new RelayCommand((obj) =>
+            {
+                AllProducts = _productService.GetAllProducts();
+            });
+
+            SelectProductCommand = new RelayCommand((obj) =>
+            {
+                var vm = new OrderProductViewModel();
+                vm.ProductInfo = SelectedProduct;
+                var view = new OrderProduct();
+                view.DataContext = vm;
+                view.ShowDialog();
+            });
+        }
+    }
+}
---
+++ StoreTaskAdoNet/StoreDB.sql
@@ -0,0 +1,58 @@
+﻿CREATE DATABASE StoreDB
+GO
+USE StoreDB
+GO
+
+CREATE TABLE Products(
+[ID] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Name] NVARCHAR(30) NOT NULL,
+[Description] NVARCHAR(30),
+[Price] MONEY NOT NULL DEFAULT(0),
+[Discount] FLOAT NOT NULL DEFAULT(0),
+[Quantity] INT NOT NULL DEFAULT(10)
+)
+
+GO
+
+CREATE TABLE Customers(
+[ID] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Username] NVARCHAR(30) NOT NULL
+)
+
+GO
+
+CREATE TABLE Orders(
+[ID] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Date] datetime2 NOT NULL DEFAULT(GETDATE()),
+[Amount] INT NOT NULL DEFAULT(1),
+[ProductID] INT FOREIGN KEY REFERENCES Products([ID]) NOT NULL,
+[CustomerID] INT FOREIGN KEY REFERENCES Customers([ID]) NOT NULL,
+)
+
+GO
+
+CREATE TABLE Admins(
+[ID] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+[Username] NVARCHAR(30) NOT NULL,
+[Password] NVARCHAR(30) NOT NULL
+)
+
+GO
+
+INSERT INTO Products([Name],[Description],[Price],[Discount],[Quantity])
+VALUES('IPhone 11 Pro', 'a phone.', 2129, 15, 250),('Geforce Rtx 4070 Ti', 'a video card.', 1999, 10, 100)
+
+GO
+
+INSERT INTO Customers([Username])
+VALUES('faridsalimov'),('emirhan')
+
+GO
+
+INSERT INTO Orders([Date],[Amount],[CustomerID],[ProductID])
+VALUES(GETDATE(), 1, 1, 1), (GETDATE(), 2, 2, 2)
+
+GO
+
+INSERT INTO Admins([Username],[Password])
+VALUES('admin', 'admin')
